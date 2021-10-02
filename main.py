0e740d99139178f3c696540720590b9b74b4a491
Dennis Mullen <dkmullen@gmail.com>
2021-10-01 20:32:32 -0400

    Add detail page

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from flask import Flask, render_template, redirect, url_for, request
 import requests
 
+app = Flask(__name__)
 base_url = "https://www.thecocktaildb.com/api/json/v1/1/"
 search_url = f"{base_url}search.php?"
 filter_url = f"{base_url}filter.php?"
@@ -10,10 +12,26 @@ list_url = f"{base_url}list.php?"
 # Will handle many (but not all) of the queries at this api
 def get_me_a_drink(url, search_string):
     drinks = requests.get(url + search_string).json()['drinks']
-    for drink in drinks:
-        print(f"{drink['strDrink']}: {drink['strGlass']}")
+    return drinks
 
 
-get_me_a_drink(base_url, 'random.php')
-# list_by_letter()
-# get_random_drink()
+@app.route("/")
+def home():
+    random_drink = get_me_a_drink(base_url, 'random.php')[0]
+    return render_template("index.html", drink=random_drink)
+
+
+@app.route("/list/<letter>")
+def list_drinks(letter):
+    drink_list = get_me_a_drink(search_url, f"f={letter}")
+    return render_template('list.html', drinks=drink_list)
+
+
+@app.route("/details/<drink_id>")
+def show_details(drink_id):
+    drink = get_me_a_drink(lookup_url, f"i={drink_id}")
+    return render_template('details.html', drink=drink)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
