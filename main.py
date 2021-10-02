0ebeee2ec4bd61e488a0fbfc8d3decd3bf1c3f2f
Dennis Mullen <dkmullen@gmail.com>
2021-10-02 14:22:16 -0400

    Parse data from api for easier use, display desired data everywhere

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ search_url = f"{base_url}search.php?"
 filter_url = f"{base_url}filter.php?"
 lookup_url = f"{base_url}lookup.php?"
 list_url = f"{base_url}list.php?"
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'v', 'w', 'y', 'z']
 
 
 # Will handle many (but not all) of the queries at this api
@@ -15,10 +17,48 @@ def get_me_a_drink(url, search_string):
     return drinks
 
 
+# Dict comprehension to clear out empty values (esp. ingredients)
+def clean_null_terms(d):
+    return {
+        k: v
+        for k, v in d.items()
+        if v is not None
+    }
+
+
+# Put ingredients and amounts into their own dictionaries
+def filter_items(search_key, d):
+    res = dict(filter(lambda item: search_key in item[0], d.items()))
+    return res
+
+
+def parse_drink_data(drink):
+    drink = clean_null_terms(drink)
+    ingredients = filter_items("strIngredient", drink)
+    amounts = filter_items("strMeasure", drink)
+
+    # Pad the amounts dict with empty values to match length of ingredients
+    counter = 1
+    while len(amounts) < len(ingredients):
+        amounts[f"placeholder{counter}"] = ''
+        counter = counter + 1
+
+    # Put ingredients and amounts together for a more useful dictionary
+    new = []
+    for (k, v), (k2, v2) in zip(ingredients.items(), amounts.items()):
+        if v2:
+            new.append({'ingredient': v, 'amount': v2})
+        else:
+            new.append({'ingredient': v, 'amount': ''})
+    drink['ingredients'] = new
+    return drink
+
+
 @app.route("/")
 def home():
-    random_drink = get_me_a_drink(base_url, 'random.php')[0]
-    return render_template("index.html", drink=random_drink)
+    drink = get_me_a_drink(base_url, 'random.php')[0]
+    random_drink = parse_drink_data(drink)
+    return render_template("index.html", random_drink=random_drink, alphabet=alphabet)
 
 
 @app.route("/list/<letter>")
@@ -29,7 +69,8 @@ def list_drinks(letter):
 
 @app.route("/details/<drink_id>")
 def show_details(drink_id):
-    drink = get_me_a_drink(lookup_url, f"i={drink_id}")
+    drink = get_me_a_drink(lookup_url, f"i={drink_id}")[0]
+    drink = parse_drink_data(drink)
     return render_template('details.html', drink=drink)
 
 
